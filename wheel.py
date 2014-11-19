6f59e331e4f25f8c00cc89f0472254687905c04c
Les Pounder <lespounder@gmail.com>
2014-11-19 15:17:48 +0000

    Added pibrella_commands.pdf with permission from Dan Aldred

--- wheel.py
+++ wheel.py
@@ -5,11 +5,11 @@ from random import randint
 def spinwheel(pin):
 	#pibrella.light.blink(0.5,0.5)
 	duration = randint(1,10)
-    print(duration)
-    pibrella.output.e.on()
-    sleep(duration)
-    pibrella.output.e.off()
-    #pibrella.light.off()
+	print(duration)
+	pibrella.output.e.on()
+	sleep(duration)
+	pibrella.output.e.off()
+	#pibrella.light.off()
 
 
 pibrella.button.pressed(spinwheel)
