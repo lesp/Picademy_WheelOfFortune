b64782de06d8d932f072d5bdc48e1ea27a7cc2ff
Les Pounder <lespounder@gmail.com>
2015-02-15 07:44:45 +0000

    Added for loop to create 5 random numbers in a row, should make the numbers less predictable

--- wheel.py
+++ wheel.py
@@ -3,13 +3,14 @@ from time import sleep
 from random import randint
 
 def spinwheel(pin):
-	#pibrella.light.blink(0.5,0.5)
-	duration = randint(1,10)
-	print(duration)
-	pibrella.output.e.on()
-	sleep(duration)
-	pibrella.output.e.off()
-	#pibrella.light.off()
+        #pibrella.light.blink(0.5,0.5)
+        for i in range(5):
+            duration = randint(1,10)
+        print(duration)
+        pibrella.output.e.on()
+        sleep(duration)
+        pibrella.output.e.off()
+        #pibrella.light.off()
 
 
 pibrella.button.pressed(spinwheel)
